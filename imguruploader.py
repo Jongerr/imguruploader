e4732ee1a934ee739829e5b1fdf38c421d9d273c
Jongerr <jontmichie@gmail.com>
2017-06-05 22:37:19 -0500

    Finished adding imgur uploading

--- imguruploader.py
+++ imguruploader.py
@@ -1,9 +1,11 @@
-import os
-import sys
 import keyboard
+import os
+import pyimgur
+import pyperclip
+import sys
 from PyQt4.QtGui import QPixmap, QApplication
 from datetime import datetime
-from config import CLIENT_ID, CLIENT_SECRET
+from config import CLIENT_ID
 
 
 def save_screen(screen_id):
@@ -13,17 +15,26 @@ def save_screen(screen_id):
     
     app = QApplication(sys.argv)
     desktop = app.desktop()
+
+    im = pyimgur.Imgur(CLIENT_ID)
     
-    screen1 = desktop.screenGeometry(0).width()
-    screen2 = desktop.screenGeometry(1).width()
-    screen3 = desktop.screenGeometry(2).width()
+    screen1width = desktop.screenGeometry(0).width()
+    screen2width = desktop.screenGeometry(1).width()
+    screen3width = desktop.screenGeometry(2).width()
 
     if screen_id == 1:
-        QPixmap.grabWindow(desktop.winId(), x=-screen3, width=screen3).save('pictures/' + filename, 'jpg')
+        QPixmap.grabWindow(desktop.winId(), x=-screen3width, width=screen3width).save('pictures/' + filename, 'jpg')
     elif screen_id == 2:
         QPixmap.grabWindow(QApplication.desktop().winId()).save('pictures/' + filename, 'jpg')
     else:
-        QPixmap.grabWindow(desktop.winId(), x=screen2).save('pictures/' + filename, 'jpg')
+        QPixmap.grabWindow(desktop.winId(), x=screen2width).save('pictures/' + filename, 'jpg')
+
+    uploaded_image = im.upload_image('pictures/' + filename, title='Thowaway Title')
+    link = uploaded_image.link
+
+    pyperclip.copy(link)
+    with open('link_log.txt', 'a') as f:
+        f.write(filename + ' --> ' + link + '\n')
         
     
 if __name__ == '__main__':
@@ -36,5 +47,4 @@ if __name__ == '__main__':
     keyboard.add_hotkey('alt+shift+1', save_screen, args=[1])
     keyboard.add_hotkey('alt+shift+2', save_screen, args=[2])
     keyboard.add_hotkey('alt+shift+3', save_screen, args=[3])
-    print('starting to wait')
     keyboard.wait('alt+shift+q')
